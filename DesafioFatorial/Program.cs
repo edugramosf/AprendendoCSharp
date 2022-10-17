f1af41432803608e01897611c6bbd98a2ef32d72
Eduardo <edugramosf@gmail.com>
2022-10-17 10:06:21 -0300

    atualização1.2

---
+++ DesafioFatorial/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+
+class ProgramaDesafioFatorial
+{
+    static void Main(String[] args)
+    {
+        int fatorial = 1;
+        for (int i = 1; i <= 10; i++)
+        {
+            fatorial *= i;
+            Console.WriteLine("Fatorial de " + i + " = " + fatorial);
+        }
+        
+    }
+}
---
+++ DesafioMultiplosDe3/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+
+class ProgramaDesafioMultiplosDe3
+{
+    static void Main(String[] args)
+    {
+        for (int i = 1; i <= 100; i++)
+        {
+            if (i % 3 == 0)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
